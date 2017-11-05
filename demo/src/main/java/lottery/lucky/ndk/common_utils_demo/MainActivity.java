88cc9c400d85d30f68a8b6d5e8e2533dbfa1af33
Banee Ishaque K <baneeishaque@gmail.com>
2017-11-05 11:37:05 +0530

    Date in words

---
+++ demo/src/main/java/lottery/lucky/ndk/common_utils_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package lottery.lucky.ndk.common_utils_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
