04e99bf23b5d85d45ad1882db5d51c53e70762d2
Banee Ishaque K <baneeishaque@gmail.com>
2017-11-08 15:33:30 +0530

    Update Utils

--- common/src/main/java/ndk/prism/common_utils/Update_Utils.java
+++ common/src/main/java/ndk/prism/common_utils/Update_Utils.java
@@ -44,7 +44,7 @@ public class Update_Utils {
 
 
     @SuppressWarnings("deprecation")
-    public static String[] get_server_version(String flavour, String full_version_check_URL) {
+    public static String[] get_flavoured_server_version(String flavour, String full_version_check_URL) {
         String network_action_response;
         try {
             // Network access.
@@ -54,6 +54,32 @@ public class Update_Utils {
             ArrayList<NameValuePair> name_pair_value = new ArrayList<>(1);
             name_pair_value.add(new BasicNameValuePair("flavour", flavour));
             http_post.setEntity(new UrlEncodedFormEntity(name_pair_value));
+
+            ResponseHandler<String> response_handler = new BasicResponseHandler();
+            network_action_response = http_client.execute(http_post, response_handler);
+            Log.d("Server Version", network_action_response);
+            return new String[]{"0", network_action_response};
+
+        } catch (UnsupportedEncodingException e) {
+            return new String[]{"1", "UnsupportedEncodingException : " + e.getLocalizedMessage()};
+        } catch (ClientProtocolException e) {
+            return new String[]{"1", "ClientProtocolException : " + e.getLocalizedMessage()};
+        } catch (IOException e) {
+            return new String[]{"1", "IOException : " + e.getLocalizedMessage()};
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public static String[] get_server_version(String full_version_check_URL) {
+        String network_action_response;
+        try {
+            // Network access.
+            DefaultHttpClient http_client = new DefaultHttpClient();
+//            HttpPost http_post = new HttpPost("http://" + General_Data.SERVER_IP_ADDRESS + "/android/get_version.php");
+            HttpPost http_post = new HttpPost(full_version_check_URL);
+//            ArrayList<NameValuePair> name_pair_value = new ArrayList<>(1);
+//            name_pair_value.add(new BasicNameValuePair("flavour", flavour));
+//            http_post.setEntity(new UrlEncodedFormEntity(name_pair_value));
 
             ResponseHandler<String> response_handler = new BasicResponseHandler();
             network_action_response = http_client.execute(http_post, response_handler);
