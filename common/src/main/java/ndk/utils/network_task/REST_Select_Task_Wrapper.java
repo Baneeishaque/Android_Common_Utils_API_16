f332c365619738882f0fa1d0a7ee0efd2317cb40
Banee Ishaque K <Baneeishaque@gmail.com>
2018-05-14 23:15:44 +0530

    fix select wrapper

--- common/src/main/java/ndk/utils/network_task/REST_Select_Task_Wrapper.java
+++ common/src/main/java/ndk/utils/network_task/REST_Select_Task_Wrapper.java
@@ -37,6 +37,18 @@ public class REST_Select_Task_Wrapper {
         }
     }
 
+    public static void execute(String task_URL, Context context, View mProgressView, View mLoginFormView, String application_Name, Pair[] name_value_pairs, REST_Select_Task.Async_Response_JSON_array_with_error_status_delegate async_response_json_array_with_error_status_delegate) {
+
+        if (isOnline(context)) {
+            ProgressBar_Utils.showProgress(true, context, mProgressView, mLoginFormView);
+            REST_Select_Task rest_select_task = new REST_Select_Task(task_URL, context, mProgressView, mLoginFormView, application_Name, name_value_pairs, async_response_json_array_with_error_status_delegate);
+
+            rest_select_task.execute();
+        } else {
+            Toast_Utils.longToast(context, "Internet is unavailable");
+        }
+    }
+
     public static void execute_splash(final Context context, final String task_URL, final String application_Name, final Pair[] name_value_pairs, final REST_Select_Task.Async_Response_JSON_array_with_error_status_delegate async_response_json_array_with_error_status_delegate) {
 
         if (Network_Utils.isOnline(context)) {
