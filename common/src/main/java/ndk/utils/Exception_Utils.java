89176e995c65a22af9bad8adc1d3fb2da6a86636
Banee Ishaque K <Baneeishaque@gmail.com>
2018-05-26 16:03:01 +0530

    Code clean

--- common/src/main/java/ndk/utils/Exception_Utils.java
+++ common/src/main/java/ndk/utils/Exception_Utils.java
@@ -1,12 +1,11 @@
 package ndk.utils;
 
-import android.content.Context;
-import android.widget.Toast;
-
 import java.util.Arrays;
 
 public class Exception_Utils {
+
     public static String get_exception_details(Exception e) {
+
         return "Exception Message : " + e.getLocalizedMessage()
                 + "\n" + "Exception Code : " + e.hashCode()
                 + "\n" + "Exception Class : " + e.getClass()
@@ -17,7 +16,4 @@ public class Exception_Utils {
 
     }
 
-    public static void display_exception_toast(Context context, Exception e) {
-        Toast.makeText(context, get_exception_details(e), Toast.LENGTH_LONG).show();
-    }
 }
