41af0a7f3504f8e90d1ba5a416a07b22c3cf56a1
Banee Ishaque K <baneeishaque@gmail.com>
2020-06-26 20:25:45 +0530

    tests

--- tests16/src/main/java/ndk/util/tests/Splash_Version_OK.java
+++ tests16/src/main/java/ndk/util/tests/Splash_Version_OK.java
@@ -13,38 +13,38 @@ import ndk.utils_android16.activities.SplashWithAutomatedUpdateActivity;
 public class Splash_Version_OK extends SplashWithAutomatedUpdateActivity {
 
     @Override
-    protected String configure_GET_CONFIGURATION_URL() {
+    public String configure_GET_CONFIGURATION_URL() {
 
         return "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server/http_API/select_Configuration.php";
     }
 
     @Override
-    protected String configure_UPDATE_URL() {
+    public String configure_UPDATE_URL() {
 
         return "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server/builds/app-debug.apk";
     }
 
     @Override
-    protected String configure_APPLICATION_NAME() {
+    public String configure_APPLICATION_NAME() {
 
         return Application_Specification.APPLICATION_NAME;
     }
 
     @Override
-    protected Class configure_NEXT_ACTIVITY_CLASS() {
+    public Class configure_NEXT_ACTIVITY_CLASS() {
         return LoginBundleActivity.class;
     }
 
     @Override
-    protected Pair[] configure_NEXT_ACTIVITY_CLASS_EXTRAS() {
+    public Pair[] configure_NEXT_ACTIVITY_CLASS_EXTRAS() {
 
-        return new Pair[] { new Pair<>("APPLICATION_NAME", Application_Specification.APPLICATION_NAME),
+        return new Pair[]{new Pair<>("APPLICATION_NAME", Application_Specification.APPLICATION_NAME),
                 new Pair<>("NEXT_ACTIVITY_CLASS", "Splash_Version_OK"), new Pair<>("SELECT_USER_URL",
-                        "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server/http_API/select_User.php") };
+                "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server/http_API/select_User.php")};
     }
 
     @Override
-    protected boolean configure_SECURITY_FLAG() {
+    public boolean configure_SECURITY_FLAG() {
 
         return BuildConfig.DEBUG;
     }
