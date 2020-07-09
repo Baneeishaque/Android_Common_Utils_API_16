e3f1eee8bef9d728c31ae5ab3afe32589afb82a7
Banee Ishaque K <baneeishaque@gmail.com>
2020-07-09 17:35:14 +0530

    api utils

--- common16/src/main/java/ndk/utils_android16/ApiUtils.java
+++ common16/src/main/java/ndk/utils_android16/ApiUtils.java
@@ -3,15 +3,18 @@ package ndk.utils_android16;
 public class ApiUtils {
 
     public static String getSubSectionedHttpApi(String section, String subSection, String methodName, String serverIpAddress, String httpApiFolder, String fileExtension) {
-        return serverIpAddress + "/" + httpApiFolder + "/" + section + "/" + subSection + "/" + methodName + fileExtension;
+
+        return getSectionedHttpApi(section, subSection + "/" + methodName, serverIpAddress, httpApiFolder, fileExtension);
     }
 
     public static String getSectionedHttpApi(String section, String methodName, String serverIpAddress, String httpApiFolder, String fileExtension) {
-        return serverIpAddress + "/" + httpApiFolder + "/" + section + "/" + methodName + fileExtension;
+
+        return getHttpApi(section + "/" + methodName, serverIpAddress, httpApiFolder, fileExtension);
     }
 
     public static String getHttpApi(String methodName, String serverIpAddress, String httpApiFolder, String fileExtension) {
-        return serverIpAddress + "/" + httpApiFolder + "/" + methodName + fileExtension;
+
+        return serverIpAddress + "/" + httpApiFolder + "/" + methodName + "."+fileExtension;
     }
 
 }
